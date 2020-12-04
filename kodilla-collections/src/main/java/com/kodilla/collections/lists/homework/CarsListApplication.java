3a60fde95eaf828f83c1775f52671f6967a7e9ee
Karolina Mirkowska <kamirkowska@gmail.com>
2020-12-05 00:03:29 +0100

    4.3

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -0,0 +1,29 @@
+package com.kodilla.collections.lists.homework;
+
+import com.kodilla.collections.arrays.homework.CarUtils;
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Chevrolet;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Opel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarsListApplication {
+    public static void main(String[] args) {
+        List<Car> cars = new ArrayList<>();
+        Ford ford = new Ford(70);
+
+        cars.add(new Opel(50));
+        cars.add(ford);
+        cars.add(new Chevrolet(100));
+
+        cars.remove(0);
+        cars.remove(ford);
+
+        System.out.println(cars.size());
+        for (Car car : cars) {
+            CarUtils.describeCar(car);
+        }
+    }
+}
